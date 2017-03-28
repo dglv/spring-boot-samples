db1a265d4123794275f78518050a3865a47ec602
dglv <ddem0025@gmail.com>
2017-03-28 14:10:20 +0300

    Added rest handlers in main.js

--- spring-boot-hsqldb-rest/src/main/webapp/resources/js/main.js
+++ spring-boot-hsqldb-rest/src/main/webapp/resources/js/main.js
@@ -1,10 +1,7 @@
 function doGet() {
-    var contextPath = document.getElementById('input-context-path').value;
-	
 	$.ajax({
         type: 'GET',
-        url: contextPath,
-        dataType: 'json',
+        url: getContextPath(),
         async: true,
         success: function(result) {
         	output(JSON.stringify(result));
@@ -16,11 +13,42 @@ function doGet() {
 }
 
 function doPost() {
-	
+	$.ajax({
+        type: 'POST',
+        url: getContextPath(),
+        dataType: 'json',
+        data: getInputData(),
+        contentType: "application/json",
+        async: true,
+        success: function(result) {
+        	output(JSON.stringify(result));
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+        	output(jqXHR.status + ' ' + jqXHR.responseText);
+        }
+    });
 }
 
 function doDelete(){
-	
+	$.ajax({
+        type: 'DELETE',
+        url: getContextPath(),
+        async: true,
+        success: function(result) {
+        	output(JSON.stringify(result));
+        },
+        error: function(jqXHR, textStatus, errorThrown) {
+        	output(jqXHR.status + ' ' + jqXHR.responseText);
+        }
+    });
+} 
+
+function getInputData(){
+	return document.getElementById('input-textarea').value;
+}
+
+function getContextPath(){
+	return document.getElementById('input-context-path').value;
 } 
 
 function output(data){
